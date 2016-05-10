fa983193731a18535078ac29e20feb77027a1f01
Anastas Fonotov <anastasf@gmail.com>
2016-05-10 15:12:55 +0300

    update

--- dist/pip-webui-settings.js
+++ dist/pip-webui-settings.js
@@ -364,97 +364,6 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
-/* global angular */
-
-(function () {
-    'use strict';
-
-    var thisModule = angular.module('pipSettings.Page', [
-        'pipState', 'pipSettings.Service', 'pipAppBar', 'pipSelected', 'pipTranslate',
-        'pipSettings.Templates'
-    ]);
-
-    thisModule.config(['pipAuthStateProvider', function (pipAuthStateProvider) {
-        pipAuthStateProvider.state('settings', {
-            url: '/settings?party_id',
-            auth: true,
-            controller: 'pipSettingsPageController',
-            templateUrl: 'settings_page/settings_page.html'
-        });
-    }]);
-
-    thisModule.controller('pipSettingsPageController', ['$scope', '$state', '$rootScope', 'pipAppBar', 'pipSettings', function ($scope, $state, $rootScope, pipAppBar, pipSettings) {
-
-        $scope.pages = _.filter(pipSettings.getPages(), function (page) {
-            if (page.visible === true && (page.access ? page.access($rootScope.$user, page) : true)) {
-                return page;
-            }
-        });
-
-        $scope.pages =_.sortBy($scope.pages, function(page){
-            return page.index;
-        });
-
-        $scope.selected = {};
-        if ($state.current.name != 'settings')
-            initSelect($state.current.name);
-        else {
-            if (pipSettings.getDefaultPage())
-                initSelect(pipSettings.getDefaultPage().state);
-            else {
-                setTimeout(function () {
-                    if (pipSettings.getDefaultPage())
-                        initSelect(pipSettings.getDefaultPage().state);
-                    else {
-                        if($scope.pages.length >0)
-                            initSelect($scope.pages[0].state);
-
-                    }
-                }, 0);
-
-            }
-
-        }
-
-        appHeader();
-
-        $scope.onNavigationSelect = onNavigationSelect;
-        $scope.onDropdownSelect = onDropdownSelect;
-
-        return;
-
-        function appHeader() {
-            pipAppBar.showMenuNavIcon();
-            pipAppBar.showTitleText('SETTINGS_TITLE');
-            pipAppBar.showLocalActions(null, []);
-            pipAppBar.hideShadow();
-        };
-
-        function onDropdownSelect(state) {
-            onNavigationSelect(state.state);
-        }
-
-        function onNavigationSelect(state) {
-            initSelect(state);
-
-            if ($scope.selected.page) {
-
-                $state.go(state);
-
-            }
-        };
-
-        function initSelect(state) {
-            $scope.selected.page = _.find($scope.pages, function (page) {
-                return page.state == state;
-            });
-            $scope.selected.pageIndex = _.indexOf($scope.pages, $scope.selected.page);
-            $scope.selected.pageId = state;
-        }
-    }]);
-
-
-})();
 (function () {
     'use strict';
 
@@ -547,6 +456,97 @@ module.run(['$templateCache', function($templateCache) {
             }
         }
     }]);
+
+})();
+/* global angular */
+
+(function () {
+    'use strict';
+
+    var thisModule = angular.module('pipSettings.Page', [
+        'pipState', 'pipSettings.Service', 'pipAppBar', 'pipSelected', 'pipTranslate',
+        'pipSettings.Templates'
+    ]);
+
+    thisModule.config(['pipAuthStateProvider', function (pipAuthStateProvider) {
+        pipAuthStateProvider.state('settings', {
+            url: '/settings?party_id',
+            auth: true,
+            controller: 'pipSettingsPageController',
+            templateUrl: 'settings_page/settings_page.html'
+        });
+    }]);
+
+    thisModule.controller('pipSettingsPageController', ['$scope', '$state', '$rootScope', 'pipAppBar', 'pipSettings', function ($scope, $state, $rootScope, pipAppBar, pipSettings) {
+
+        $scope.pages = _.filter(pipSettings.getPages(), function (page) {
+            if (page.visible === true && (page.access ? page.access($rootScope.$user, page) : true)) {
+                return page;
+            }
+        });
+
+        $scope.pages =_.sortBy($scope.pages, function(page){
+            return page.index;
+        });
+
+        $scope.selected = {};
+        if ($state.current.name != 'settings')
+            initSelect($state.current.name);
+        else {
+            if (pipSettings.getDefaultPage())
+                initSelect(pipSettings.getDefaultPage().state);
+            else {
+                setTimeout(function () {
+                    if (pipSettings.getDefaultPage())
+                        initSelect(pipSettings.getDefaultPage().state);
+                    else {
+                        if($scope.pages.length >0)
+                            initSelect($scope.pages[0].state);
+
+                    }
+                }, 0);
+
+            }
+
+        }
+
+        appHeader();
+
+        $scope.onNavigationSelect = onNavigationSelect;
+        $scope.onDropdownSelect = onDropdownSelect;
+
+        return;
+
+        function appHeader() {
+            pipAppBar.showMenuNavIcon();
+            pipAppBar.showTitleText('SETTINGS_TITLE');
+            pipAppBar.showLocalActions(null, []);
+            pipAppBar.hideShadow();
+        };
+
+        function onDropdownSelect(state) {
+            onNavigationSelect(state.state);
+        }
+
+        function onNavigationSelect(state) {
+            initSelect(state);
+
+            if ($scope.selected.page) {
+
+                $state.go(state);
+
+            }
+        };
+
+        function initSelect(state) {
+            $scope.selected.page = _.find($scope.pages, function (page) {
+                return page.state == state;
+            });
+            $scope.selected.pageIndex = _.indexOf($scope.pages, $scope.selected.page);
+            $scope.selected.pageId = state;
+        }
+    }]);
+
 
 })();
 /**
