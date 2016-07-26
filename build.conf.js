2cde44dad01e514d329e5b788e54fbe8c05fdc93
Sergey Seroukhov <seroukhov@gmail.com>
2016-07-25 20:02:45 -0700

    Changed references

--- build.conf.js
+++ build.conf.js
@@ -11,6 +11,28 @@ module.exports = {
         lib: true,
         images: true
     },
+    file: {
+        import: [
+            '../pip-webui-test/dist/**/*',
+            '../pip-webui-lib/dist/**/*',
+            '../pip-webui-css/dist/**/*',
+            '../pip-webui-core/dist/**/*',
+            '../pip-webui-rest/dist/**/*',
+            '../pip-webui-controls/dist/**/*',
+            '../pip-webui-nav/dist/**/*',
+            '../pip-webui-layouts/dist/**/*',
+            '../pip-webui-pictures/dist/**/*',
+            '../pip-webui-locations/dist/**/*',
+            '../pip-webui-documents/dist/**/*',
+            '../pip-webui-composite/dist/**/*',
+            '../pip-webui-errors/dist/**/*',
+            '../pip-webui-entry/dist/**/*',
+            '../pip-webui-settings/dist/**/*',
+            //'../pip-webui-guidance/dist/**/*',
+            // '../pip-webui-support/dist/**/*',
+            //'../pip-webui-help/dist/**/*'
+        ]
+    },
     samples: {
         port: 8030,
         publish: {
