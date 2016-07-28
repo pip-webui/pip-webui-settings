b5769e0c08134ab60137c8ffb6e8738741890e3e
Sergey Seroukhov <seroukhov@gmail.com>
2016-07-28 11:18:10 -0700

    Update build.conf.js

--- build.conf.js
+++ build.conf.js
@@ -1,7 +1,7 @@
 module.exports = {
     module: {
         name: 'pipSettings',
-        index: 'settings'
+        styles: 'settings'
     },
     build: {
         js: true,
@@ -9,10 +9,11 @@ module.exports = {
         html: true,
         css: true,
         lib: true,
-        images: true
+        images: true,
+        dist: false
     },
     file: {
-        import: [
+        lib: [
             '../pip-webui-test/dist/**/*',
             '../pip-webui-lib/dist/**/*',
             '../pip-webui-css/dist/**/*',
@@ -32,5 +33,11 @@ module.exports = {
             // '../pip-webui-support/dist/**/*',
             //'../pip-webui-help/dist/**/*'
         ]
+    },
+    samples: {
+        port: 8150
+    },
+    api: {
+        port: 8151
     }
 };
