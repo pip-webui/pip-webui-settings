520073c0cd138acd6b613aa37b39924364de901d
dvoikinalex <94dvoikin@gmail.com>
2017-02-23 20:23:20 +0300

    Updated submodules

--- dist/pip-webui-settings.d.ts
+++ dist/pip-webui-settings.d.ts
@@ -7,6 +7,12 @@ function configureSettingsPageRoutes($stateProvider: any): void;
 
 
 
+
+
+
+
+
+
 export class SettingsTab {
     state: string;
     title: string;
@@ -36,12 +42,6 @@ export class SettingsConfig {
     titleLogo: boolean;
     isNavIcon: boolean;
 }
-
-
-
-
-
-
 
 }
 
