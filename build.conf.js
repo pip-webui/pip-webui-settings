4e9ea7827f02864f650a3c8be6a7b80f8d75c43c
KateNegrienko <katya@negrienko.com>
2017-03-02 13:23:49 +0200

    Migrations

--- build.conf.js
+++ build.conf.js
@@ -11,7 +11,7 @@ module.exports = {
         tsd: true,
         bundle: true,
         html: true,
-        less: true,
+        sass: true,
         lib: true,
         images: true,
         dist: false
